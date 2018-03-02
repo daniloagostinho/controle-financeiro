f8d6e03b51d3fcb182d2ea6b8c4b63d960edc27b
daniloagostinho <danilo091011>
2018-03-02 14:48:30 -0300

    convertendo projeto css em scss

--- src/app/cadastro-usuarios/cadastro-usuarios.component.ts
+++ src/app/cadastro-usuarios/cadastro-usuarios.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cadastro-usuarios',
   templateUrl: './cadastro-usuarios.component.html',
-  styleUrls: ['./cadastro-usuarios.component.css']
+  styleUrls: ['./cadastro-usuarios.component.scss']
 })
 export class CadastroUsuariosComponent implements OnInit {
-
+  titulo = 'Cadastro de usuário';
   constructor() { }
 
   ngOnInit() {
   }
+
+  escolheDados(dados) {
+    switch(dados) {
+      case 'Carlos':
+      return 'purple'
+
+      case 'Juliete':
+      return 'red'
+    }
+  }
+
+  dados: any[] = [
+    {
+      "name": "Juliete",
+       "idade": 28
+    },
+    {
+      "name": "Carlos",
+       "idade": 18
+    }
+  ]
 
 }
